abc5a9ea0cf965eefec87696b53bbeb6ba01d76a
olanipekun abiodun <olanipekunabbey@gmail.com>
2026-01-29 00:47:50 -0800

    first commit

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -1,7 +1,11 @@
+import Icon from "../assets/bakery48.png";
+
 export default function Logo() {
   return (
     <div className="flex items-center gap-2">
-      <span className="text-3xl">🧁</span>
+      <span className="text-3xl">
+        <img src={`${Icon}`} alt="" />
+      </span>
       <h1 className="text-xl font-bold tracking-wide">Top Choice Bakery</h1>
     </div>
   );
